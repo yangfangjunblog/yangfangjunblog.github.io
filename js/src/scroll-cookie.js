e5a9017224618dee1b64bbf5d6bbe4f0193574b2
yangfangjunblog <leo_yang215@163.com>
2020-01-09 14:05:45 +0800

    Site updated: 2020-01-09 14:05:38

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 09 2020 14:00:57 GMT+0800 (GMT+08:00)
+// build time:Thu Jan 09 2020 14:05:31 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 09 2020 14:00:59 GMT+0800 (GMT+08:00)
+// build time:Thu Jan 09 2020 14:05:33 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
