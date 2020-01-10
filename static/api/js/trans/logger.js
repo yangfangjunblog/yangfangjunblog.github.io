950ae004a792c25d3b2c46b89c7aac5c66e024c1
yangfangjunblog <leo_yang215@163.com>
2020-01-10 10:46:18 +0800

    Site updated: 2020-01-10 10:46:12

--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 10 2020 00:06:02 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 10 2020 10:46:07 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
--- static/api/js/trans/trans_bdxc.js
+++ static/api/js/trans/trans_bdxc.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 10 2020 00:06:02 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 10 2020 10:46:07 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("trans/trans_bdxc",function(e,t){var n=function(){var e,t=window,n=document,a="_bdXC";t[a]?window._bdXC_loaded&&t[a].reInit():(e=n.createElement("script"),e.setAttribute("charset","utf-8"),e.src="http://xiangce.baidu.com/zt/collect/mark.js?"+(new Date).getTime(),n.getElementsByTagName("head")[0].appendChild(e))};t.run=n});
 //rebuild by neat 
