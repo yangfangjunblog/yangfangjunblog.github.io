e8a189835b617b7dfada05f8c5b56c158988b28f
yangfangjunblog <leo_yang215@163.com>
2020-01-10 11:13:54 +0800

    Site updated: 2020-01-10 11:13:49

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 10 2020 11:09:55 GMT+0800 (GMT+08:00)
+// build time:Fri Jan 10 2020 11:13:45 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=(n("base/tangram").T,n("base/class").Class),s=n("share/api_base");a.CombineApi=e.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},s.ApiBase)});
 //rebuild by neat 
