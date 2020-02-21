62908fe847eb2cae310cb4efe8f050a3ceb08165
yangfangjunblog <leo_yang215@163.com>
2020-02-21 15:17:12 +0800

    Site updated: 2020-02-21 15:17:01

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 21 2020 14:54:13 GMT+0800 (GMT+08:00)
+// build time:Fri Feb 21 2020 15:16:54 GMT+0800 (GMT+08:00)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
